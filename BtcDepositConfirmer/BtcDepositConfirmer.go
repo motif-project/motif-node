35fff6de07c4def6657e811582036c46d05df8cd
ahmad ashraf <ahmadashraf250@gmai.com>
2024-11-06 03:37:45 +0500

    removing logs

--- BtcDepositConfirmer/BtcDepositConfirmer.go
+++ BtcDepositConfirmer/BtcDepositConfirmer.go
@@ -2,7 +2,6 @@ package BtcDepositConfirmer
 
 import (
 	"fmt"
-	"log"
 	"math/big"
 	"time"
 
@@ -20,54 +19,32 @@ func CheckDeposit() {
 	for {
 		depositRequests := db.QueryDepositRequests(dbconn)
 
-		if len(depositRequests) <= 0 {
-			fmt.Println("no deposit requests found")
-			time.Sleep(3 * time.Minute)
-			continue
-		}
-
 		for _, request := range depositRequests {
-			fmt.Println("found deposit requests")
-			fmt.Println("request: ", request.TransactionID)
 			tx, err := utils.GetRawTransaction(request.TransactionID)
 			if err != nil {
-				fmt.Println("Failed to get raw transaction: ", err)
 				continue
 			}
-			fmt.Println("found raw tx")
 			if tx.Confirmations <= 2 {
 				continue
 			}
 
-			fmt.Println("tx confirmed : ", tx.Hex)
-
-			t, err := utils.CreateTxFromHex(tx.Hex)
+			transaction, err := utils.CreateTxFromHex(tx.Hex)
 			if err != nil {
 				fmt.Println("Failed to create tx from hex: ", err)
+				continue
 			}
-
-			fmt.Println("tx: ", t)
-			fmt.Println(t.TxOut[1])
-
-			_, addresses, _, err := txscript.ExtractPkScriptAddrs(t.TxOut[1].PkScript, &chaincfg.SigNetParams)
-			if err != nil {
-				log.Fatalf("Failed to extract addresses: %v", err)
-			}
-
-			fmt.Println("============")
-			fmt.Println("addresses: ", addresses)
 
 			multisigaddresses := db.QueryMultisigAddresses(dbconn)
 
 			for _, multiSigAddress := range multisigaddresses {
-				fmt.Println("multisig found")
-				fmt.Println("multisig address: ", multiSigAddress.Address)
-				for _, txOut := range tx.Vout {
-					fmt.Println("checking txout : ", txOut)
-					fmt.Println("checking txout  hex: ", txOut.ScriptPubKey.Hex)
-					for _, address := range txOut.ScriptPubKey.Addresses {
-						fmt.Println("checking address: ", address)
-						if multiSigAddress.Address == address {
+				for _, txOut := range transaction.TxOut {
+					_, addresses, _, err := txscript.ExtractPkScriptAddrs(txOut.PkScript, &chaincfg.SigNetParams)
+					if err != nil {
+						fmt.Println("Failed to extract addresses: ", err)
+						continue
+					}
+					for _, address := range addresses {
+						if multiSigAddress.Address == address.EncodeAddress() {
 							fmt.Println("found my deposit requests")
 							bigInt := new(big.Int)
 							_, success := bigInt.SetString(request.Amount, 10)
