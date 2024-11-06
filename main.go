80199740936113bfa1d4d660b6995701cd89c743
ahmad ashraf <ahmadashraf250@gmai.com>
2024-11-06 05:22:35 +0500

    removing logs

--- main.go
+++ main.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 
 	"github.com/AhmadAshraf2/Judge-AVS/BtcDepositConfirmer"
-	"github.com/AhmadAshraf2/Judge-AVS/PodManager"
 	"github.com/AhmadAshraf2/Judge-AVS/api"
 	"github.com/AhmadAshraf2/Judge-AVS/ethComms"
 	"github.com/AhmadAshraf2/Judge-AVS/operator"
@@ -34,16 +31,6 @@ func main() {
 	go BtcDepositConfirmer.CheckDeposit()
 	wg.Add(1)
 	go ethComms.SubscribeToDepositRequests()
-
-	request := &PodManager.PodManagerBitcoinWithdrawalPSBTRequest{
-		Pod:             common.HexToAddress("0x365119a2De4e389B542AAe945f26cb21cA3f3Fbf"),
-		Operator:        common.HexToAddress("0x60b3b41240Fb9d353acE1E37B0CE79054154eA40"),
-		WithdrawAddress: []byte{205, 219, 47, 200, 4, 245, 199, 159, 16, 228, 183, 54, 241, 116, 5, 224, 234, 118, 227, 29},
-		Raw:             types.Log{},
-	}
-
-	ethComms.HandleWithdrawalRequest(request)
-
 	ethComms.SubscribeToWithdrawRequests()
 	wg.Wait()
 }
