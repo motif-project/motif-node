08876b6a68875738cc44f025c8987e78f80c5eb7
ahmad ashraf <ahmadashraf250@gmai.com>
2024-12-19 20:45:53 +0500

    simple bug fix

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func initialize() {
 		fmt.Println("Invalid environment")
 		panic("Invalid environment")
 	}
+
 	utils.LoadBtcWallet(viper.GetString("wallet_name"))
 	ethAccount := ethComms.LoadEthAccount()
 	fmt.Println("Eth account: ", ethAccount.Address.Hex())
